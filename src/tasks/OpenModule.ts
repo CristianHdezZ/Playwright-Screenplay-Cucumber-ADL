96089d84ecc0524a8d464f065f663f03fdd5903b
Cristian  Hernandez Zabala <ingcristianhernandez@hotmail.com>
2025-10-25 00:35:24 -0500

    Test Execute Regresion

--- src/tasks/OpenModule.ts
+++ src/tasks/OpenModule.ts
@@ -5,13 +5,13 @@ import { HomePage } from '../ui/HomePage';
 
 export class OpenModule {
     static called(nombreModulo: string) {
-        Wait.for(Duration.ofSeconds(55));
+        Wait.for(Duration.ofSeconds(120));
         return Task.where(`#open the module ${nombreModulo}`,
             Click.on(HomePage.BUTTON_MODULO(nombreModulo)),
 
             // Espera opcional para asegurar carga completa
-            Interaction.where(`#wait 5 seconds`, async () => {
-                await new Promise(resolve => setTimeout(resolve, 10000));
+            Interaction.where(`#wait 15 seconds`, async () => {
+                await new Promise(resolve => setTimeout(resolve, 15000));
             }),
             
         );        
