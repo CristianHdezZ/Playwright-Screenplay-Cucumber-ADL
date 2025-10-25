e49ccb9856abea93d541379e1d9817924f919bc0
Cristian  Hernandez Zabala <ingcristianhernandez@hotmail.com>
2025-10-25 02:22:25 -0500

    Test Execute Regresion-I

--- src/tasks/OpenModule.ts
+++ src/tasks/OpenModule.ts
@@ -1,16 +1,17 @@
 import { Task, Interaction, Wait, Duration } from '@serenity-js/core';
-import { Click, isVisible } from '@serenity-js/web';
+import { Click, isVisible, Scroll } from '@serenity-js/web';
 import { HomePage } from '../ui/HomePage';
 
 
 export class OpenModule {
     static called(nombreModulo: string) {
-        Wait.for(Duration.ofSeconds(20));
-        return Task.where(`#open the module ${nombreModulo}`,
-            //Wait.until(HomePage.BUTTON_MODULO(nombreModulo), isVisible()),
-            //Wait.for(Duration.ofSeconds(10)),
-            Click.on(HomePage.BUTTON_MODULO(nombreModulo)),
+       
+        return Task.where(`#open the module ${nombreModulo}`,           
 
+            Wait.for(Duration.ofSeconds(8)), // espera general para asegurar renderizado
+            Scroll.to(HomePage.BUTTON_MODULO(nombreModulo)),          
+            Wait.until(HomePage.BUTTON_MODULO(nombreModulo), isVisible()),
+            Click.on(HomePage.BUTTON_MODULO(nombreModulo)),
             // Espera opcional para asegurar carga completa
             Interaction.where(`#wait 5 seconds`, async () => {
                 await new Promise(resolve => setTimeout(resolve, 5000));
