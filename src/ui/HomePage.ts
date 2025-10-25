4c3e517dad78bce030e00e94fb3e933e1d7db5e8
Cristian  Hernandez Zabala <ingcristianhernandez@hotmail.com>
2025-10-25 02:08:53 -0500

    Test Execute Regresion

--- src/ui/HomePage.ts
+++ src/ui/HomePage.ts
@@ -5,7 +5,9 @@ export class HomePage {
   static readonly MAIN_CONTENT_PERFIL = '(*//div//img[@class="user-avatar"]//..//div/p[@class="text-footer"])[2]';
   static BUTTON_MODULO(nombreModulo: string) {
         return PageElement.located(
-            By.xpath(`(//a[(text())='${nombreModulo}'])[2]`)
+            By.xpath(`(//a[contains(text(),'${nombreModulo}')])[2]`)
         ).describedAs(`botón del módulo ${nombreModulo}`);
+
+        
     }
 }
